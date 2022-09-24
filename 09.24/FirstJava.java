d0c6be9b03deb9d79a67a871e001ad446c9efb63
mduong1701 <minhmanlk0411@gmail.com>
2022-09-24 01:06:25 -0700

    Initial commit

---
+++ 09.24/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Man Duong");
+        System.out.println("I am 18 years old");
+        System.out.println("My hometown is Long Khanh, Dong Nai, Vietnam");
+    }
+}
